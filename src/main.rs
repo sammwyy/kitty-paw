4d92721ef01edd44598743d615eb9337409c9f43
Sammwy <sammwy.dev@gmail.com>
2022-10-15 04:29:50 -0300

    VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
 #![no_std] // Don't link rust std library.
 #![no_main] // Disable rust-level entry points.
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Kitty Paw";
+// Entrypoint since the linker looks for a function
+// named '_start' by default.
 
 #[no_mangle] // Don't mangle the name of this fn.
 pub extern "C" fn _start() -> ! {
-    // Entrypoint since the linker looks for a function
-    // named '_start' by default.
-
-    // VGA rendering.
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Intentional crash");
 
     // Block with a infinite loop.
-    loop {}
+    // loop {}
 }
 
 // Function called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("Kernal Panic: {}", info);
     loop {}
 }
